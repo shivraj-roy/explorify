0c429634c9ce3c94f54560d8ba448a7daa8e219c
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-19 20:20:51 +0530

    ⚒︎ Refactor Box and NavBar components: adjust width properties for better layout consistency...

--- UI/Box.tsx
+++ UI/Box.tsx
@@ -6,7 +6,7 @@ type BoxPropTypes = {
 const Box = ({ children, boxClass }: BoxPropTypes) => {
    return (
       <div className={`flex items-center justify-center ${boxClass}`}>
-         <div className="p-8 rounded-lg shadow-md w-[350] md:w-[400] max-w-md backdrop">
+         <div className="p-8 rounded-lg shadow-md w-[320] md:w-[400] max-w-md backdrop">
             {children}
          </div>
       </div>
